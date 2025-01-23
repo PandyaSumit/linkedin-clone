16e75c3a5f94b30de0cdd6541a94a6f68505628a
Sumit Pandya <pandyasumit01@gmail.com>
2025-01-23 18:58:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Let build Linkedin clone</h1>
+
+       {/* Header */}
+       {/* App Body */}
+       {/* SideBar */}
+       {/* Feed */}
+       {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
